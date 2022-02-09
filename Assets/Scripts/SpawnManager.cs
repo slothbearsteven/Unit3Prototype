d7123385c2637e9f1e851d4c432b76426bffb9c5
Steven Kennedy <umbrylax@gmail.com>
2022-02-09 16:18:49 -0700

    enabled spawn intervals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0.5f, 0);
+    private Vector3 spawnPos = new Vector3(25, 0.3f, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+    void SpawnObstacle()
+    {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
-
     // Update is called once per frame
     void Update()
     {
