0b18f617d450b1772683c77045aa6ed59dbbef69
Steven Kennedy <umbrylax@gmail.com>
2022-02-10 14:25:30 -0700

    Obstacles succesfully delete after exiting bounds

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
     private PlayerController playerControllerScript;
+    private float leftBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,10 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 }
